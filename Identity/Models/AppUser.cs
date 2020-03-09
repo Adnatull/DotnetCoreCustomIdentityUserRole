1b8a392cbce2fd758423b6562b0506e03b90f9e9
Adnatull <adnaan.masum@yahoo.com>
2020-03-09 11:59:52 +0600

    IsEnabled field

--- Identity/Models/AppUser.cs
+++ Identity/Models/AppUser.cs
@@ -11,5 +11,6 @@ namespace Identity.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public virtual ICollection<AppRole> UserRoles { get; set; }
+        public bool? IsEnabled { get; set; }
     }
 }
